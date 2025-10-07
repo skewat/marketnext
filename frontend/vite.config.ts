72f68b5e0b174c7f5f913863412905c387fcf006
Chandrakant Kewat <ckewat@ubuntu-cloudpanel.localhost>
2025-10-07 23:26:40 +0530

    restore host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+  // Add your desired hostname to the list of allowed hosts to prevent DNS rebinding attacks.
+    host: true,
+    allowedHosts: [
+      'marketnext.in'
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:6123',
